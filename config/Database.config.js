59c17d9cf1abe9990b572652b56753fa58c736cb
qrsolution <service@qrsolution.fr>
2024-07-09 01:36:11 +0100

    Update Database.config.js

--- config/Database.config.js
+++ config/Database.config.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://PDF01:lm3SxPP9ahk4owsH@cluster0.dtutsqg.mongodb.net/work",
+      "mongodb+srv://service:jawher123@cluster0.fuplva1.mongodb.net/qrsolution",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
